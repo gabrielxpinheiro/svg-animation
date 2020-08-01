f34c913e5e28aa46d26c8547650eaccf95408f9e
Gabriel Pinheiro <gabrielpinheiro77@gmail.com>
2020-08-01 13:23:36 -0300

    animation footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 import { Cards } from "./Cards.js";
-import { Icones} from "./Icones.js";
+import { Icones } from "./Icones.js";
+import { Footer } from "./Footer.js";
 
 window.onload = () => {
-  let animaCards = new Cards();
+  const animaCards = new Cards();
   document.addEventListener("scroll", animaCards.scrollCards.bind(animaCards));
 
   const icones = new Icones();
-  icones.animaIcones()
+  icones.animaIcones();
+
+  const footer = new Footer();
+  footer.efeitoOnda()
 };
